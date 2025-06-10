22fcdd978282a70691560f0b53cb0c0910ff3f4f
Hanny658 <hannyanhai@gmail.com>
2025-06-10 15:39:06 +1000

    remove options in auth from export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
 import { compare, hash } from "bcryptjs";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
     session: {
         strategy: "jwt",
         maxAge: 60 * 60 * 24 * 14,      // fortnight expiry
