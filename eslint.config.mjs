e214c1d48dbd6d76f33fee3ef8dbdb89b71a5c03
Hanny658 <hannyanhai@gmail.com>
2025-06-13 16:24:26 +1000

    Clearing out rest files for snackmap

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,9 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  {
-    ignores: ["src/generated/prisma/**"],
-  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
