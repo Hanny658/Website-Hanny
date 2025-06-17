92571769f1f4b5e7374992f89744340d5d7d17f1
Hanny658 <hannyanhai@gmail.com>
2025-06-17 17:12:08 +1000

    Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from '../components/navbar'
 import "./globals.css";
@@ -20,6 +20,11 @@ export const metadata: Metadata = {
   title: "Hanny",
   description: "Hanny's personal website using next.ts",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 
 export default function RootLayout({
   children,
